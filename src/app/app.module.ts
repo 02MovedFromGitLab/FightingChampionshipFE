6147a387c5655baf6cf65b4f127eebcab988dd64
Graham Duthie <grahamd483@gmail.com>
2021-07-14 13:06:24 +0200

    initial commit BS FA Popper Jquery and all components models and services made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FighterListComponent } from './components/fighter-list/fighter-list.component';
+import { FighterFormComponent } from './components/fighter-form/fighter-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FighterListComponent,
+    FighterFormComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule
